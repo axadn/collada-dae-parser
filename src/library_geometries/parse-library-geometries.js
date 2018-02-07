63451545f0b1ac856a172755d264ac38fcd1b416
Adan Marrufo <adanmarrufo@gmail.com>
2018-02-06 23:16:03 -0800

    make vertex data order flexible, add vertex colors to parse-library-geometries.js

--- src/library_geometries/parse-library-geometries.js
+++ src/library_geometries/parse-library-geometries.js
@@ -11,40 +11,49 @@ function ParseLibraryGeometries (library_geometries) {
 
   var geometryMesh = library_geometries[0].geometry[0].mesh[0]
   var source = geometryMesh.source
-
   /* Vertex Positions, UVs, Normals */
   var polylistIndices = (geometryMesh.polylist || geometryMesh.triangles)[0].p[0].split(' ')
 
-  var vertexNormalIndices = []
-  var vertexPositionIndices = []
-  var vertexUVIndices = []
-  // TODO: This is currently dependent on a certain vertex data order
-  // we should instead read this order from the .dae file
+//vertex data order is now flexible, vertex colors have been added 
+  const polylistInputs = (geometryMesh.polylist || geometryMesh.triangles)[0].input.map(el=> el.$)
+
+  const offsetOutputMap = {};
+  const semantics = ["VERTEX", "NORMAL", "COLOR", "TEXCOORD"]
+  const outputs = {}
+  semantics.forEach(semantic=>{
+    outputs[semantic] = {indices: [], elements: []}
+  });
+  polylistInputs.forEach(input =>{
+    offsetOutputMap[input.offset] = input.semantic;
+    if(outputs[input.semantic]){
+      outputs[input.semantic].elements =
+        (semantic=>{
+          let dataSource;
+          dataSource = source.find(sourceElement=> sourceElement.$.id ==input.source.slice(1));
+          if(!dataSource && semantic == "VERTEX"){
+            dataSource = geometryMesh.vertices.find(sourceElement=> sourceElement.$.id == input.source.slice(1))
+            dataSource = source.find(sourceElement=> sourceElement.$.id = dataSource.input[0].$.source.slice(1))
+          }
+          return dataSource;
+        })(input.semantic).float_array[0]._.split(' ').map(Number)
+    }
+  });
+
   polylistIndices.forEach(function (vertexIndex, positionInArray) {
-    if (positionInArray % source.length === 0) {
-      vertexPositionIndices.push(Number(vertexIndex))
-    } else if (positionInArray % source.length === 1) {
-      vertexNormalIndices.push(Number(vertexIndex))
+      if(offsetOutputMap[positionInArray % source.length]){
+      outputs[offsetOutputMap[positionInArray % source.length]].indices.push(Number(vertexIndex));
     }
-    if (positionInArray % source.length === 2) {
-      vertexUVIndices.push(Number(vertexIndex))
-    }
-  })
-  var vertexPositions = source[0].float_array[0]._.split(' ').map(Number)
-  var vertexNormals = source[1].float_array[0]._.split(' ').map(Number)
-  var vertexUVs = []
-  // TODO: use input, semantics, source, offset, etc
-  if (source[2]) {
-    vertexUVs = source[2].float_array[0]._.split(' ').map(Number)
-  }
-  /* End Vertex Positions, UVs, Normals */
+  });
 
   return {
-    vertexPositions: vertexPositions,
-    vertexNormals: vertexNormals,
-    vertexUVs: vertexUVs,
-    vertexNormalIndices: vertexNormalIndices,
-    vertexPositionIndices: vertexPositionIndices,
-    vertexUVIndices: vertexUVIndices
+    vertexPositions: outputs.VERTEX.elements,
+    vertexNormals: outputs.NORMAL.elements,
+    vertexUVs: outputs.TEXCOORD.elements,
+    vertexColors: outputs.COLOR.elements,
+    vertexNormalIndices: outputs.NORMAL.indices,
+    vertexPositionIndices: outputs.VERTEX.indices,
+    vertexUVIndices: outputs.TEXCOORD.indices,
+    vertexColorIndices: outputs.COLOR.indices
   }
 }
+
